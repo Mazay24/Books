3b085e5b83fda4a6e6dcb2c3e83b99c453a55d7d
Исаев Никита <nik.isaev2004@mail.ru>
2025-04-03 16:52:40 +0300

    Initial frontend commit

---
+++ Books/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿
+namespace Books
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dgvBooks = new System.Windows.Forms.DataGridView();
+            this.btnAddBook = new System.Windows.Forms.Button();
+            this.btnEditBook = new System.Windows.Forms.Button();
+            this.btnDeleteBook = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dgvReaders = new System.Windows.Forms.DataGridView();
+            this.btnAddReader = new System.Windows.Forms.Button();
+            this.btnEditReader = new System.Windows.Forms.Button();
+            this.btnDeleteReader = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dgvBorrowRecords = new System.Windows.Forms.DataGridView();
+            this.btnBorrowBook = new System.Windows.Forms.Button();
+            this.btnReturnBook = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnFilterBorrowed = new System.Windows.Forms.Button();
+            this.btnFilterAvailable = new System.Windows.Forms.Button();
+            this.btnClearSearch = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReaders)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBorrowRecords)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 50);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 400);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dgvBooks);
+            this.tabPage1.Controls.Add(this.btnAddBook);
+            this.tabPage1.Controls.Add(this.btnEditBook);
+            this.tabPage1.Controls.Add(this.btnDeleteBook);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 374);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Книги";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dgvBooks
+            // 
+            this.dgvBooks.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBooks.Location = new System.Drawing.Point(6, 6);
+            this.dgvBooks.Name = "dgvBooks";
+            this.dgvBooks.Size = new System.Drawing.Size(680, 362);
+            this.dgvBooks.TabIndex = 0;
+            // 
+            // btnAddBook
+            // 
+            this.btnAddBook.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddBook.Location = new System.Drawing.Point(692, 6);
+            this.btnAddBook.Name = "btnAddBook";
+            this.btnAddBook.Size = new System.Drawing.Size(94, 30);
+            this.btnAddBook.TabIndex = 1;
+            this.btnAddBook.Text = "Добавить";
+            this.btnAddBook.UseVisualStyleBackColor = true;
+            this.btnAddBook.Click += new System.EventHandler(this.btnAddBook_Click);
+            // 
+            // btnEditBook
+            // 
+            this.btnEditBook.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEditBook.Location = new System.Drawing.Point(692, 42);
+            this.btnEditBook.Name = "btnEditBook";
+            this.btnEditBook.Size = new System.Drawing.Size(94, 30);
+            this.btnEditBook.TabIndex = 2;
+            this.btnEditBook.Text = "Редактировать";
+            this.btnEditBook.UseVisualStyleBackColor = true;
+            this.btnEditBook.Click += new System.EventHandler(this.btnEditBook_Click);
+            // 
+            // btnDeleteBook
+            // 
+            this.btnDeleteBook.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDeleteBook.Location = new System.Drawing.Point(692, 78);
+            this.btnDeleteBook.Name = "btnDeleteBook";
+            this.btnDeleteBook.Size = new System.Drawing.Size(94, 30);
+            this.btnDeleteBook.TabIndex = 3;
+            this.btnDeleteBook.Text = "Удалить";
+            this.btnDeleteBook.UseVisualStyleBackColor = true;
+            this.btnDeleteBook.Click += new System.EventHandler(this.btnDeleteBook_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dgvReaders);
+            this.tabPage2.Controls.Add(this.btnAddReader);
+            this.tabPage2.Controls.Add(this.btnEditReader);
+            this.tabPage2.Controls.Add(this.btnDeleteReader);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 374);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Читатели";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dgvReaders
+            // 
+            this.dgvReaders.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvReaders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReaders.Location = new System.Drawing.Point(6, 6);
+            this.dgvReaders.Name = "dgvReaders";
+            this.dgvReaders.Size = new System.Drawing.Size(680, 362);
+            this.dgvReaders.TabIndex = 1;
+            // 
+            // btnAddReader
+            // 
+            this.btnAddReader.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddReader.Location = new System.Drawing.Point(692, 6);
+            this.btnAddReader.Name = "btnAddReader";
+            this.btnAddReader.Size = new System.Drawing.Size(94, 30);
+            this.btnAddReader.TabIndex = 2;
+            this.btnAddReader.Text = "Добавить";
+            this.btnAddReader.UseVisualStyleBackColor = true;
+            this.btnAddReader.Click += new System.EventHandler(this.btnAddReader_Click);
+            // 
+            // btnEditReader
+            // 
+            this.btnEditReader.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEditReader.Location = new System.Drawing.Point(692, 42);
+            this.btnEditReader.Name = "btnEditReader";
+            this.btnEditReader.Size = new System.Drawing.Size(94, 30);
+            this.btnEditReader.TabIndex = 3;
+            this.btnEditReader.Text = "Редактировать";
+            this.btnEditReader.UseVisualStyleBackColor = true;
+            this.btnEditReader.Click += new System.EventHandler(this.btnEditReader_Click);
+            // 
+            // btnDeleteReader
+            // 
+            this.btnDeleteReader.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDeleteReader.Location = new System.Drawing.Point(692, 78);
+            this.btnDeleteReader.Name = "btnDeleteReader";
+            this.btnDeleteReader.Size = new System.Drawing.Size(94, 30);
+            this.btnDeleteReader.TabIndex = 4;
+            this.btnDeleteReader.Text = "Удалить";
+            this.btnDeleteReader.UseVisualStyleBackColor = true;
+            this.btnDeleteReader.Click += new System.EventHandler(this.btnDeleteReader_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dgvBorrowRecords);
+            this.tabPage3.Controls.Add(this.btnBorrowBook);
+            this.tabPage3.Controls.Add(this.btnReturnBook);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(792, 374);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Выдачи";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dgvBorrowRecords
+            // 
+            this.dgvBorrowRecords.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvBorrowRecords.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBorrowRecords.Location = new System.Drawing.Point(6, 6);
+            this.dgvBorrowRecords.Name = "dgvBorrowRecords";
+            this.dgvBorrowRecords.Size = new System.Drawing.Size(680, 362);
+            this.dgvBorrowRecords.TabIndex = 2;
+            // 
+            // btnBorrowBook
+            // 
+            this.btnBorrowBook.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBorrowBook.Location = new System.Drawing.Point(692, 6);
+            this.btnBorrowBook.Name = "btnBorrowBook";
+            this.btnBorrowBook.Size = new System.Drawing.Size(94, 30);
+            this.btnBorrowBook.TabIndex = 3;
+            this.btnBorrowBook.Text = "Выдать";
+            this.btnBorrowBook.UseVisualStyleBackColor = true;
+            this.btnBorrowBook.Click += new System.EventHandler(this.btnBorrowBook_Click);
+            // 
+            // btnReturnBook
+            // 
+            this.btnReturnBook.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReturnBook.Location = new System.Drawing.Point(692, 42);
+            this.btnReturnBook.Name = "btnReturnBook";
+            this.btnReturnBook.Size = new System.Drawing.Size(94, 30);
+            this.btnReturnBook.TabIndex = 4;
+            this.btnReturnBook.Text = "Вернуть";
+            this.btnReturnBook.UseVisualStyleBackColor = true;
+            this.btnReturnBook.Click += new System.EventHandler(this.btnReturnBook_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnFilterBorrowed);
+            this.panel1.Controls.Add(this.btnFilterAvailable);
+            this.panel1.Controls.Add(this.btnClearSearch);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.txtSearch);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 50);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnFilterBorrowed
+            // 
+            this.btnFilterBorrowed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFilterBorrowed.Location = new System.Drawing.Point(692, 12);
+            this.btnFilterBorrowed.Name = "btnFilterBorrowed";
+            this.btnFilterBorrowed.Size = new System.Drawing.Size(96, 23);
+            this.btnFilterBorrowed.TabIndex = 5;
+            this.btnFilterBorrowed.Text = "Выданные";
+            this.btnFilterBorrowed.UseVisualStyleBackColor = true;
+            this.btnFilterBorrowed.Click += new System.EventHandler(this.btnFilterBorrowed_Click);
+            // 
+            // btnFilterAvailable
+            // 
+            this.btnFilterAvailable.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFilterAvailable.Location = new System.Drawing.Point(590, 12);
+            this.btnFilterAvailable.Name = "btnFilterAvailable";
+            this.btnFilterAvailable.Size = new System.Drawing.Size(96, 23);
+            this.btnFilterAvailable.TabIndex = 4;
+            this.btnFilterAvailable.Text = "Доступные";
+            this.btnFilterAvailable.UseVisualStyleBackColor = true;
+            this.btnFilterAvailable.Click += new System.EventHandler(this.btnFilterAvailable_Click);
+            // 
+            // btnClearSearch
+            // 
+            this.btnClearSearch.Location = new System.Drawing.Point(300, 12);
+            this.btnClearSearch.Name = "btnClearSearch";
+            this.btnClearSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnClearSearch.TabIndex = 3;
+            this.btnClearSearch.Text = "Очистить";
+            this.btnClearSearch.UseVisualStyleBackColor = true;
+            this.btnClearSearch.Click += new System.EventHandler(this.btnClearSearch_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(219, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Поиск";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(60, 14);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(153, 20);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Поиск:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "Система управления библиотекой";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReaders)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBorrowRecords)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+      
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dgvBooks;
+        private System.Windows.Forms.Button btnAddBook;
+        private System.Windows.Forms.Button btnEditBook;
+        private System.Windows.Forms.Button btnDeleteBook;
+        private System.Windows.Forms.DataGridView dgvReaders;
+        private System.Windows.Forms.Button btnAddReader;
+        private System.Windows.Forms.Button btnEditReader;
+        private System.Windows.Forms.Button btnDeleteReader;
+        private System.Windows.Forms.DataGridView dgvBorrowRecords;
+        private System.Windows.Forms.Button btnBorrowBook;
+        private System.Windows.Forms.Button btnReturnBook;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClearSearch;
+        private System.Windows.Forms.Button btnFilterAvailable;
+        private System.Windows.Forms.Button btnFilterBorrowed;
+
+
+    
+
+    #endregion
+}
+    }
